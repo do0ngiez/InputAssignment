a8e31c8f1580b3cba2e7f067857205c4cb0e7a83
do0ngiez <yujmin901205@gmail.com>
2024-10-05 11:11:52 +0800

    Input Assignment

--- Program.cs
+++ Program.cs
@@ -4,14 +4,26 @@ using System.IO;
 namespace NumberLogger{
     class Program{
         static void Main(string[] args){
+
+            //ask and read user input
             Console.WriteLine("Enter a number: ");
             string userInput = Console.ReadLine();
+
+            //specify file pathj
             string fp = "loggedNumber.txt";
+
+            //call the method to write the user's input to the text file
             LogNumberToFile(userInput, fp);
+
+            //call method to print the context of a text file
             PrintFileContents(fp);
         }
+        
 
+        //method to write the user's input to the text file
         static void LogNumberToFile(string number, string fp){
+            
+            //use StreamWriter and write the user's input to the file
             using (StreamWriter writer = new StreamWriter(fp)){
                 writer.WriteLine(number);
             }
@@ -19,6 +31,7 @@ namespace NumberLogger{
            // Console.WriteLine("Number written in file.");
         }
 
+        //method that prints the contents of the text file
         static void PrintFileContents(string fp){
             if(File.Exists(fp)){
                 using (StreamReader reader = new StreamReader(fp)){
