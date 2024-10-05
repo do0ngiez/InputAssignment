d7643f2467e86f0646d34892c7b338ebb08143c4
Hexabyte <jiminie.aespa@gmail.com>
2024-10-05 11:06:30 +0800

    Input Assignment

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace NumberLogger{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Enter a number: ");
+            string userInput = Console.ReadLine();
+            string fp = "loggedNumber.txt";
+            LogNumberToFile(userInput, fp);
+            PrintFileContents(fp);
+        }
+
+        static void LogNumberToFile(string number, string fp){
+            using (StreamWriter writer = new StreamWriter(fp)){
+                writer.WriteLine(number);
+            }
+
+           // Console.WriteLine("Number written in file.");
+        }
+
+        static void PrintFileContents(string fp){
+            if(File.Exists(fp)){
+                using (StreamReader reader = new StreamReader(fp)){
+                    
+                    string fileContent = reader.ReadToEnd();
+                    Console.WriteLine("Contents of the file:");
+                    Console.WriteLine(fileContent);
+                }
+            }else{
+                Console.WriteLine("File not found");
+            }
+        }
+    }
+}
